219b50ba4ea114929bc4ec8e707cba2614526ea6
Karan <kumarkay.ca@gmail.com>
2019-11-06 14:07:27 -0500

    Initial Commit

---
+++ CameraImagePicker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CameraImagePicker
+//
+//  Created by Sandeep Jangra on 2019-11-06.
+//  Copyright © 2019 Karan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
